2df44478b13f9653b3e36f279b77b348d378e2d5
Peaky00 <chris.kirkley@yahoo.com>
2023-11-25 00:46:55 -0600

    can't fix errors starting a new one

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
 const express = require('express');
-const passport = require('passport');
 const session = require('express-session');
-const flash = require('connect-flash');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./Public/config/database'); // Assuming you've configured your database connection
+const path = require('path');
 
 const app = express();
 
-// Initialize Passport
-app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash()); // For displaying flash messages
+// Middleware for parsing request body (Define this before using it)
+function myMiddleware(req, res, next) {
+    // Your middleware logic here
+    console.log('Middleware executed');
+    next(); // Don't forget to call next to pass control to the next middleware or route handler.
+}
 
+// Use the middleware in your Express app
+app.use(myMiddleware);
+
+// Define your routes and handle requests
 app.get('/', (req, res) => {
-    res.render('home', { layout: 'layouts/main' });
+    res.send('Hello, Express!');
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up session management
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+});
+app.use(
+  session({
+    secret: 'your-secret-key', // Replace with a secure secret key
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set 'views' directory for your HTML files
+app.set('views', path.join(__dirname, 'views'));
+
+// Set the view engine to 'ejs' instead of 'html'
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// Define your routes
+const authRoutes = require('./controllers/authController');
+const blogRoutes = require('./controllers/blogController');
+
+app.use('/', authRoutes);
+app.use('/blog', blogRoutes);
+
+// Start the Express.js server
+const PORT = process.env.PORT || 3000;
+sequelize
+  .sync() // Sync the database models with the actual database tables
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database synchronization error:', error);
   });
-// Set up a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, world!'); // Send a simple response
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
